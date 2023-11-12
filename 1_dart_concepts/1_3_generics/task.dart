d85eb38d5e563a0370e883ed2466f696c1fb4fbe
hazget <hazget91@gmail.com>
2023-11-12 15:56:18 +0300

    1_dart_concepts/1_3_generics 1_dart_concepts/1_4_conditional_compilation

--- 1_dart_concepts/1_3_generics/task.dart
+++ 1_dart_concepts/1_3_generics/task.dart
@@ -1,4 +1,35 @@
-void main() {
-  // Implement a method, returning the maximum element from a `Comparable` list.
-  // You must use generics to allow different types usage with that method.
+// void main() {
+//   // Implement a method, returning the maximum element from a `Comparable` list.
+//   // You must use generics to allow different types usage with that method.
+// }
+
+T findMax<T extends Comparable>(List<T> list) {
+  if (list.isEmpty) {
+    throw ArgumentError('The list cannot be empty.');
+  }
+
+  T max = list[0];
+
+  for (var element in list) {
+    if (element.compareTo(max) > 0) {
+      max = element;
+    }
+  }
+
+  return max;
 }
+
+void main() {
+  List<int> intList = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5];
+  int maxInt = findMax(intList);
+  print('Максимальное целое число: $maxInt');
+
+  List<double> doubleList = [3.14, 2.71, 1.62, 2.5, 1.0];
+  double maxDouble = findMax(doubleList);
+  print('Максимальное вещественное число: $maxDouble');
+
+  List<String> stringList = ['apple', 'banana', 'cherry', 'date'];
+  String maxString = findMax(stringList);
+  print('Максимальная строка: $maxString');
+}
+
--- 1_dart_concepts/1_4_conditional_compilation/task.dart
+++ 1_dart_concepts/1_4_conditional_compilation/task.dart
@@ -1,5 +1,23 @@
+// void main() {
+//   // Create a native and web implementations for a custom [DateTime], supporting
+//   // microseconds. Use conditional compilation to export the class for general
+//   // use on any platform.
+// }
+class MyDateTime {
+ final DateTime _dateTime;
+
+ MyDateTime(this._dateTime);
+
+ int get microseconds => _dateTime.microsecondsSinceEpoch;
+
+ String format(String formatString) {
+    return _dateTime.toString();
+ }
+  
+}
 void main() {
-  // Create a native and web implementations for a custom [DateTime], supporting
-  // microseconds. Use conditional compilation to export the class for general
-  // use on any platform.
+ MyDateTime myDateTime = MyDateTime(DateTime.now());
+
+ print('Microseconds since epoch: ${myDateTime.microseconds}');
+ print('Formatted date: ${myDateTime.format('yyyy-MM-dd HH:mm:ss')}');
 }
