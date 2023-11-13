4cacdb2a2f0cc4521ce55bfa9a91d6990bff2946
hazget <hazget91@gmail.com>
2023-11-13 18:56:31 +0300

    1_dart_concepts re-wrote

--- 1_dart_concepts/1_1_mixins/task_2.dart
+++ 1_dart_concepts/1_1_mixins/task_2.dart
@@ -1,62 +1,61 @@
-// void main() {
-//   // Implement an extension on [String], parsing links from a text.
-//   //
-//   // Extension should return a [List] of texts and links, e.g.:
-//   // - `Hello, google.com, yay` ->
-//   //   [Text('Hello, '), Link('google.com'), Text(', yay')].
-// }
-extension ParseLinks on String {
-  List<dynamic> parseLinks() {
-    final List<dynamic> result = [];
-    final RegExp linkRegex = RegExp(r'https?://\S+');
+extension StringLinkParser on String {
+  List<Linkable> parseLinks() {
+    final List<Linkable> result = [];
+    final UriMatcher uriMatcher = UriMatcher();
 
-    var remainingText = this;
-    while (remainingText.isNotEmpty) {
-      final RegExpMatch? match = linkRegex.firstMatch(remainingText);
+    final matches = uriMatcher.allMatches(this);
 
-      if (match != null) {
-        // Found a link
-        final linkText = match.group(0);
-        final textBeforeLink = remainingText.substring(0, match.start);
-        
-        if (textBeforeLink.isNotEmpty) {
-          result.add(textBeforeLink);
-        }
-        result.add(Link(linkText!));
+    int start = 0;
 
-        // Update the remaining text
-        remainingText = remainingText.substring(match.end);
-      } else {
-        // No more links found
-        result.add(remainingText);
-        break;
+    for (final match in matches) {
+      if (match.start > start) {
+        result.add(Text(this.substring(start, match.start)));
       }
+
+      result.add(Link(this.substring(match.start, match.end)));
+
+      start = match.end;
+    }
+
+    if (start < length) {
+      result.add(Text(this.substring(start)));
     }
 
     return result;
   }
 }
 
-class Text {
+class UriMatcher {
+  static final RegExp _uriRegex =
+      RegExp(r'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\\(\\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+');
+
+  Iterable<Match> allMatches(String input) => _uriRegex.allMatches(input);
+}
+
+abstract class Linkable {}
+
+class Text implements Linkable {
   final String text;
+
   Text(this.text);
 }
 
-class Link {
-  final String link;
-  Link(this.link);
+class Link implements Linkable {
+  final String url;
+
+  Link(this.url);
 }
 
 void main() {
-  final inputText = 'Hello, check out https://www.google.com, yay, and also visit https://www.example.com';
-  final parsedList = inputText.parseLinks();
-
-  for (final item in parsedList) {
+  final textWithLinks = 'Hello, check out https://google.com or visit https://example.com, yay';
+  
+  final parsedItems = textWithLinks.parseLinks();
+  
+  for (final item in parsedItems) {
     if (item is Text) {
       print('Text: ${item.text}');
     } else if (item is Link) {
-      print('Link: ${item.link}');
+      print('Link: ${item.url}');
     }
   }
 }
-
--- 1_dart_concepts/1_1_mixins/task_3.dart
+++ 1_dart_concepts/1_1_mixins/task_3.dart
@@ -1,36 +1,14 @@
 /// Object equipable by a [Character].
 abstract class Item {}
 
-/// Mixin for [Item], representing a weapon.
+/// Mixin for weapons, providing damage property.
 mixin Weapon on Item {
   int get damage;
 }
 
-/// Mixin for [Item], representing armor.
+/// Mixin for armor, providing defense property.
 mixin Armor on Item {
   int get defense;
-}
-
-/// [Item] representing a sword.
-class Sword extends Item with Weapon {
-  @override
-  int get damage => 10;
-}
-
-/// [Item] representing a shield.
-class Shield extends Item with Armor {
-  @override
-  int get defense => 5;
-}
-
-/// Enum representing character slots.
-enum CharacterSlot {
-  leftHand,
-  rightHand,
-  hat,
-  torso,
-  legs,
-  shoes,
 }
 
 /// Entity equipping [Item]s.
@@ -48,62 +26,98 @@ class Character {
 
   /// Returns the total damage of this [Character].
   int get damage {
-    return equipped.whereType<Weapon>().map((item) => item.damage).fold(0, (a, b) => a + b);
+    return equipped.whereType<Weapon>().map((weapon) => weapon.damage).fold(0, (a, b) => a + b);
   }
 
   /// Returns the total defense of this [Character].
   int get defense {
-    return equipped.whereType<Armor>().map((item) => item.defense).fold(0, (a, b) => a + b);
+    return equipped.whereType<Armor>().map((armor) => armor.defense).fold(0, (a, b) => a + b);
   }
 
-  /// Equips the provided [item], putting it in the corresponding slot.
+  /// Equips the provided [item], meaning putting it to the corresponding slot.
   ///
-  /// If the slot is already occupied, throws an [OverflowException].
-  void equip(Item item, CharacterSlot slot) {
-    switch (slot) {
-      case CharacterSlot.leftHand:
-        if (leftHand != null) throw OverflowException();
+  /// If there's already a slot occupied, then throws a [OverflowException].
+  void equip(Item item) {
+    if (item is Weapon && (leftHand == null || rightHand == null)) {
+      if (leftHand == null) {
         leftHand = item;
-        break;
-      case CharacterSlot.rightHand:
-        if (rightHand != null) throw OverflowException();
+      } else {
         rightHand = item;
-        break;
-      case CharacterSlot.hat:
-        if (hat != null) throw OverflowException();
-        hat = item;
-        break;
-      case CharacterSlot.torso:
-        if (torso != null) throw OverflowException();
-        torso = item;
-        break;
-      case CharacterSlot.legs:
-        if (legs != null) throw OverflowException();
-        legs = item;
-        break;
-      case CharacterSlot.shoes:
-        if (shoes != null) throw OverflowException();
-        shoes = item;
-        break;
+      }
+    } else if (item is Armor) {
+      switch (item.runtimeType) {
+        case Hat:
+          hat = item;
+          break;
+        case Torso:
+          torso = item;
+          break;
+        case Legs:
+          legs = item;
+          break;
+        case Shoes:
+          shoes = item;
+          break;
+      }
+    } else {
+      throw OverflowException();
     }
   }
 }
 
 /// [Exception] indicating there's no place left in the [Character]'s slot.
-class OverflowException implements Exception {}
+class OverflowException implements Exception {
+   @override
+  String toString() => 'No place left in the Character\'s slot.';
+}
+
+/// Example classes implementing the mixins
+class Sword extends Item with Weapon {
+  @override
+  int get damage => 10;
+}
+
+class Helmet extends Item with Armor {
+  @override
+  int get defense => 5;
+}
+
+class Hat extends Item with Armor {
+  @override
+  int get defense => 2;
+}
+
+class Torso extends Item with Armor {
+  @override
+  int get defense => 8;
+}
+
+class Legs extends Item with Armor {
+  @override
+  int get defense => 7;
+}
+
+class Shoes extends Item with Armor {
+  @override
+  int get defense => 3;
+}
 
 void main() {
-  final sword = Sword();
-  final shield = Shield();
   final character = Character();
+  final sword = Sword();
+  final helmet = Helmet();
+  final hat = Hat();
+  final torso = Torso();
+  final legs = Legs();
+  final shoes = Shoes();
 
-  try {
-    character.equip(sword, CharacterSlot.leftHand);
-    character.equip(shield, CharacterSlot.leftHand); // This will throw OverflowException.
-  } catch (e) {
-    print('Error: $e');
-  }
+  character.equip(sword);
+  character.equip(helmet);
+  character.equip(hat);
+  character.equip(torso);
+  character.equip(legs);
+  character.equip(shoes);
 
-  print('Total Damage: ${character.damage}');
-  print('Total Defense: ${character.defense}');
+  print('Total damage: ${character.damage}');
+  print('Total defense: ${character.defense}');
 }
