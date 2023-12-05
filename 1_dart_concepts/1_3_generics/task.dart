42493fad036a2481337d9e6c7c7a8e9410a89332
hazget <hazget91@gmail.com>
2023-12-06 02:09:50 +0300

    new

--- 1_dart_concepts/1_3_generics/task.dart
+++ 1_dart_concepts/1_3_generics/task.dart
@@ -1,6 +1,6 @@
-T findMaxElement<T extends Comparable<dynamic>>(List<T> list) {
+T findMax<T extends Comparable>(List<T> list) {
   if (list.isEmpty) {
-    throw ArgumentError('List cannot be null or empty');
+    throw ArgumentError('List cannot be empty');
   }
 
   T maxElement = list[0];
@@ -14,9 +14,15 @@ T findMaxElement<T extends Comparable<dynamic>>(List<T> list) {
 }
 
 void main() {
-  List<int> intList = [1, 3, 5, 7, 9, 6, 11, 2];
-  List<String> stringList = ['apple', 'banana', 'cherry', 'null', 'empty'];
+  List<int> intList = [1, 5, 3, 2, 4];
+  int maxInt = findMax(intList);
+  print('Max int: $maxInt');
 
-  print(findMaxElement(intList)); 
-  print(findMaxElement(stringList)); 
+  List<double> doubleList = [1.5, 3.2, 5.4, 2.7, 4.1];
+  double maxDouble = findMax(doubleList);
+  print('Max double: $maxDouble');
+
+  List<String> stringList = ['apple', 'banana', 'pineapple', 'orange'];
+  String maxString = findMax(stringList);
+  print('Max string: $maxString');
 }
