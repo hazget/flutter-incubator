a60c475425e8bddb5bae4a1b9fcb793a608d86c9
hazget <hazget91@gmail.com>
2023-12-07 23:58:04 +0300

    optimization

--- 1_dart_concepts/1_2_async/task_3.dart
+++ 1_dart_concepts/1_2_async/task_3.dart
@@ -1,61 +1,44 @@
-import 'dart:convert';
 import 'dart:io';
-
-void main() async {
-  final server = await HttpServer.bind('127.0.0.1', 8080);
-  print('Server running on ${server.address}:${server.port}');
-
-  await for (var request in server) {
-    if (request.method == 'POST') {
-      // Handle POST request to write to dummy.txt
-      await handleWriteRequest(request);
-    } else if (request.method == 'GET') {
-      // Handle GET request to read from dummy.txt
-      await handleReadRequest(request);
-    } else {
-      // Respond with Method Not Allowed for other request types
-      request.response
-        ..statusCode = HttpStatus.methodNotAllowed
-        ..write('Method Not Allowed')
-        ..close();
-    }
-  }
-}
+import 'dart:convert';
 
 Future<void> handleWriteRequest(HttpRequest request) async {
-  try {
-    // Read the request body
-    final body = await utf8.decoder.bind(request).join();
-    // Write to dummy.txt
-    await File('dummy.txt').writeAsString(body);
-    // Respond with success
-    request.response
-      ..statusCode = HttpStatus.ok
-      ..write('Data written to dummy.txt')
-      ..close();
-  } catch (e) {
-    // Respond with Internal Server Error if an error occurs
-    request.response
-      ..statusCode = HttpStatus.internalServerError
-      ..write('Internal Server Error')
-      ..close();
-  }
+  final file = File('dummy.txt');
+  final content = await utf8.decoder.bind(request).join();
+  await file.writeAsString(content);
+
+  request.response.statusCode = HttpStatus.ok;
+  request.response.write('File written successfully');
+  await request.response.close();
 }
 
 Future<void> handleReadRequest(HttpRequest request) async {
-  try {
-    // Read from dummy.txt
-    final content = await File('dummy.txt').readAsString();
-    // Respond with the file contents
-    request.response
-      ..statusCode = HttpStatus.ok
-      ..write(content)
-      ..close();
-  } catch (e) {
-    // Respond with Not Found if dummy.txt does not exist
-    request.response
-      ..statusCode = HttpStatus.notFound
-      ..write('File Not Found')
-      ..close();
+  final file = File('dummy.txt');
+  if (!await file.exists()) {
+    request.response.statusCode = HttpStatus.notFound;
+    request.response.write('File not found');
+    await request.response.close();
+    return;
+  }
+
+  final content = await file.readAsString();
+  request.response.statusCode = HttpStatus.ok;
+  request.response.write(content);
+  await request.response.close();
+}
+
+void main() async {
+  final server = await HttpServer.bind('localhost', 0);
+  print('Server listening on localhost:${server.port}');
+
+  await for (var request in server) {
+    if (request.method == 'POST' && request.uri.path == '/write') {
+      await handleWriteRequest(request);
+    } else if (request.method == 'GET' && request.uri.path == '/read') {
+      await handleReadRequest(request);
+    } else {
+      request.response.statusCode = HttpStatus.notFound;
+      request.response.write('Not found');
+      await request.response.close();
+    }
   }
 }
